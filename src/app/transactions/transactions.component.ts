9ac9c9ea9f1dff49580834be3ebb7ba70d7e7af8
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2025-01-27 18:56:37 +0200

    corrected pagination for responsive design

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import {
   MatPaginator,
@@ -28,65 +28,68 @@ import {
     MatIconModule,
   ],
   templateUrl: './transactions.component.html',
-  styleUrl: './transactions.component.scss',
+  styleUrls: ['./transactions.component.scss'],
 })
 export class TransactionsComponent implements OnInit {
+  svgSortMobile: string = 'assets/icon-sort-mobile.svg';
   selectedTransaction: string | undefined;
   selectedTimeline: string | undefined;
-  pages: number[] = [];
+  pages: (number | string)[] = [];
   timeLines: { id: number; value: string }[] = [
-    {
-      id: 1,
-      value: 'Latest',
-    },
-    {
-      id: 2,
-      value: 'Oldest',
-    },
+    { id: 1, value: 'Latest' },
+    { id: 2, value: 'Oldest' },
   ];
-
   categories: { id: number; value: string }[] = [
-    {
-      id: 3,
-      value: 'All Transactions',
-    },
-    {
-      id: 4,
-      value: 'Small Transactions',
-    },
-    {
-      id: 5,
-      value: 'Large Transactions',
-    },
+    { id: 3, value: 'All Transactions' },
+    { id: 4, value: 'Small Transactions' },
+    { id: 5, value: 'Large Transactions' },
   ];
   sidenavOpen: boolean = false;
   transactions: Transaction[] = [];
   filteredTransactions: Transaction[] = [];
   type: any;
+  selectedTimeLine: string | null = null;
+  selectedCategory: string | null = null;
+
+  @ViewChild('sortSelect') sortSelect!: MatSelect;
+  @ViewChild('categorySelect') categorySelect!: MatSelect;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  paginatedData: any = [];
+  pageSize = 10;
+  totalPages = 0;
+  currentPage = 0;
 
   constructor(private transactionService: TransactionsService) {}
 
-  @ViewChild(MatPaginator)
-  paginator!: MatPaginator;
+  ngOnInit(): void {
+    this.showPagination();
+    this.showData();
+  }
+
+  showData() {
+    this.transactionService.getTransactions().subscribe((data) => {
+      this.transactions = data[0]['transactions'];
+      this.filteredTransactions = this.transactions;
+      this.updatePagination();
+      this.updatePageData();
+    });
+  }
 
   onPageChange(event: PageEvent) {
     this.pageSize = event.pageSize;
     this.updatePageData(event.pageIndex);
+    this.showPagination();
   }
 
   updatePagination() {
     this.totalPages = Math.ceil(
       this.filteredTransactions.length / this.pageSize
     );
-
-    this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1);
-
-    if (this.totalPages === 0) {
-      this.pages = [];
-    }
+    this.showPagination();
   }
 
-  updatePageData(pageIndex = 0) {
+  updatePageData(pageIndex = this.currentPage) {
     const startIndex = pageIndex * this.pageSize;
     this.paginatedData = this.filteredTransactions.slice(
       startIndex,
@@ -95,15 +98,11 @@ export class TransactionsComponent implements OnInit {
     this.currentPage = pageIndex;
   }
 
-  paginatedData: any = [];
-  pageSize = 10;
-  totalPages = Math.ceil(this.transactions.length / this.pageSize);
-  currentPage = 0;
-
   onPrevPage() {
     if (this.currentPage > 0) {
       this.currentPage--;
       this.updatePageData(this.currentPage);
+      this.showPagination();
     }
   }
 
@@ -111,21 +110,8 @@ export class TransactionsComponent implements OnInit {
     if (this.currentPage < this.totalPages - 1) {
       this.currentPage++;
       this.updatePageData(this.currentPage);
+      this.showPagination();
     }
-  }
-
-  showData() {
-    this.transactionService.getTransactions().subscribe((data) => {
-      this.transactions = data[0]['transactions'];
-      this.filteredTransactions = this.transactions;
-      this.totalPages = Math.ceil(this.transactions.length / this.pageSize);
-      this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1);
-      this.updatePageData();
-    });
-  }
-
-  ngOnInit(): void {
-    this.showData();
   }
 
   filterTable(event: Event) {
@@ -134,55 +120,77 @@ export class TransactionsComponent implements OnInit {
     this.filteredTransactions = this.transactions.filter((item) =>
       item.name.toLowerCase().includes(value)
     );
-
     this.currentPage = 0;
-
-    this.totalPages = Math.ceil(
-      this.filteredTransactions.length / this.pageSize
-    );
-
-    this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+    this.updatePagination();
     this.updatePageData();
   }
 
   showPage(pageNumber: number): void {
     this.updatePageData(pageNumber);
+    this.showPagination();
   }
 
   onTimelineChange(selectedTimeline: string) {
+    this.selectedTimeline = selectedTimeline;
     if (selectedTimeline === 'Latest') {
-      this.filteredTransactions.sort((a, b) => {
-        return new Date(b.date).getTime() - new Date(a.date).getTime();
-      });
+      this.filteredTransactions.sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
     } else {
-      this.filteredTransactions.sort((a, b) => {
-        return new Date(a.date).getTime() - new Date(b.date).getTime();
-      });
+      this.filteredTransactions.sort(
+        (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
+      );
     }
     this.updatePagination();
     this.updatePageData();
   }
 
   onCategoryChange(selectedCategory: string) {
+    this.selectedCategory = selectedCategory;
     if (selectedCategory === 'All Transactions') {
       this.filteredTransactions = [...this.transactions];
     } else if (selectedCategory === 'Small Transactions') {
-      this.filteredTransactions = this.transactions.filter((item) => {
-        return item.amount < 0 && item.amount >= -50;
-      });
+      this.filteredTransactions = this.transactions.filter(
+        (item) => item.amount < 0 && item.amount >= -50
+      );
     } else if (selectedCategory === 'Large Transactions') {
-      this.filteredTransactions = this.transactions.filter((item) => {
-        return item.amount >= 50 || item.amount < -50;
-      });
+      this.filteredTransactions = this.transactions.filter(
+        (item) => item.amount >= 50 || item.amount < -50
+      );
     }
-
     this.filteredTransactions.sort((a, b) => b.amount - a.amount);
-
     if (selectedCategory === 'Small Transactions') {
       this.filteredTransactions.sort((a, b) => a.amount - b.amount);
     }
-
     this.updatePagination();
     this.updatePageData();
+  }
+
+  showPagination() {
+    if (this.totalPages <= 4) {
+      this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+    } else {
+      if (this.currentPage === 0) {
+        this.pages = [1, 2, 3, 4];
+      } else if (this.currentPage === 1) {
+        this.pages = [1, 2, 3, '...'];
+      } else if (this.currentPage === 2) {
+        this.pages = [1, 2, 3, '...'];
+      } else if (this.currentPage === this.totalPages - 2) {
+        this.pages = [1, '...', this.totalPages - 2, this.totalPages - 1];
+      } else if (this.currentPage === this.totalPages - 1) {
+        this.pages = [
+          '...',
+          this.totalPages - 2,
+          this.totalPages - 1,
+          this.totalPages,
+        ];
+        console.log('is this the step');
+      } else if (this.currentPage === this.totalPages - 1) {
+        this.pages = [1, 2, '...'];
+      } else {
+        this.pages = [1, '...', this.currentPage - 1];
+      }
+    }
   }
 }
