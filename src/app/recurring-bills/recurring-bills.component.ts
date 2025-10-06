362c9895e7a82a1d37720ab16d788bd8cc5482f7
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2025-10-06 17:15:42 +0300

    added bills images

--- src/app/recurring-bills/recurring-bills.component.ts
+++ src/app/recurring-bills/recurring-bills.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { BillsService } from './bills.service';
 import { recurringBills } from './recurringBills.interface';
-import { MatSelect, MatSelectModule } from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recurring-bills',
   standalone: true,
-  imports: [MatSelectModule],
+  imports: [CommonModule, MatSelectModule],
   templateUrl: './recurring-bills.component.html',
   styleUrl: './recurring-bills.component.scss',
 })
 export class RecurringBillsComponent implements OnInit {
   totalBills: number = 0;
   billsUrl: string = 'assets/recurring-bills.svg';
+  allBills: recurringBills[] = [];
 
   constructor(private billsService: BillsService) {}
 
@@ -24,9 +26,25 @@ export class RecurringBillsComponent implements OnInit {
     this.billsService
       .getBillsTotalValue()
       .subscribe((bills: recurringBills[]) => {
-        console.log(bills);
-
+        this.allBills = bills;
+        console.log(this.allBills);
         this.totalBills = bills.reduce((prev, cur) => prev + cur.amount, 0);
       });
+  }
+
+  getAvatarUrl(title: string): string {
+    const map: Record<string, string> = {
+      Elevate: 'Elevate.png',
+      Spark: 'Spark.png',
+      Serenity: 'Serenity.png',
+      Pixel: 'Pixel.png',
+      Nimbus: 'Nimbus.png',
+      ByteWise: 'ByteWise.png',
+      EcoFuel: 'EcoFuel.png',
+      AquaFlow: 'AquaFlow.png',
+    };
+
+    const key = Object.keys(map).find((k) => title.includes(k));
+    return key ? `assets/${map[key]}` : 'assets/Elevate.png';
   }
 }
--- src/app/recurring-bills/recurringBills.interface.ts
+++ src/app/recurring-bills/recurringBills.interface.ts
@@ -1,5 +1,6 @@
 export interface recurringBills {
   id: number;
+  _id: string;
   avatar: string;
   title: string;
   dueDate: string;
