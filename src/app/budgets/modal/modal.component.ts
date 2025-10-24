cd92c6cc35d22b9e7dea9ce80af96465632e8721
Ferenc Deak <deak_ferenc_jozsef@yahoo.com>
2025-10-24 18:18:53 +0300

    done budgets page pots is still to be done

--- src/app/budgets/modal/modal.component.ts
+++ src/app/budgets/modal/modal.component.ts
@@ -298,8 +298,7 @@ export class ModalComponent implements OnInit {
       ? Number(selAmountInput.value)
       : 0;
 
-    const newBudget: Budget = {
-      id: this.selectedCategory.id,
+    const newBudget: Partial<Budget> = {
       category: this.selectedCategory.category,
       amount: amountValue,
       theme: this.selectedCategory.theme,
@@ -307,7 +306,7 @@ export class ModalComponent implements OnInit {
       optional: false,
     };
 
-    this.modalService.addBudget(newBudget).subscribe({
+    this.modalService.addBudget(newBudget as Budget).subscribe({
       next: (response: Budget | null) => {
         if (!response) return;
 
